53420aba9161978cb22e07cbfe564095b0ae9446
antonzhao <519102870@qq.com>
2023-08-06 03:30:21 +0800

    feat: 新增work，包裹线程和selector

---
+++ src/main/java/com/antonzhao/TestServer.java
@@ -0,0 +1,56 @@
+package com.antonzhao;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.nio.charset.Charset;
+import java.util.Iterator;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
+public class TestServer {
+    private static final Logger logger = LoggerFactory.getLogger(TestServer.class);
+
+    public static void main(String[] args) throws IOException {
+        ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
+        serverSocketChannel.configureBlocking(false);
+
+        Selector selector = Selector.open();
+        SelectionKey selectionKey = serverSocketChannel.register(selector, 0, serverSocketChannel);
+        selectionKey.interestOps(SelectionKey.OP_ACCEPT);
+        serverSocketChannel.bind(new InetSocketAddress(8080));
+
+        Work work = new Work();
+        while (true) {
+            logger.info("main函数阻塞在这里吧。。。。。。。");
+            selector.select();
+            Set<SelectionKey> selectionKeys = selector.selectedKeys();
+            Iterator<SelectionKey> keyIterator = selectionKeys.iterator();
+            while (keyIterator.hasNext()) {
+                SelectionKey key = keyIterator.next();
+                keyIterator.remove();
+                if (key.isAcceptable()) {
+                    ServerSocketChannel channel = (ServerSocketChannel) key.channel();
+                    // 得到客户端的channel
+                    SocketChannel socketChannel = channel.accept();
+                    work.register(socketChannel);
+//                    socketChannel.configureBlocking(false);
+//                    SelectionKey socketChannelKey = socketChannel.register(workSelector, 0, socketChannel);
+//                    socketChannelKey.interestOps(SelectionKey.OP_READ);
+//                    work.start();
+                    logger.info("客户端在main函数中连接成功！");
+                    //连接成功之后，用客户端的channel写回一条消息
+                    socketChannel.write(ByteBuffer.wrap("客户端发送成功了".getBytes()));
+                    logger.info("main函数服务器向客户端发送数据成功！");
+                }
+            }
+        }
+    }
+}
